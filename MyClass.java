13d5204f5f6f5abe8c2d7a6ccf4650e5e6a37256
Maneesh Kumar <maneesh02125@gmail.com>
2025-04-13 14:03:37 +0530

    basic structure of java

---
+++ MyClass.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+
+        // int a = myScanner.nextInt();
+
+        // System.out.println(a);
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+
+    }
+}
